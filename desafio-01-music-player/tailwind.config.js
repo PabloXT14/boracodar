4d885c2a464a8653199998e8a56357c2a6932ca3
Pablo Alan <pabloxt14@gmail.com>
2024-03-08 15:00:00 -0300

    feat: (challenge 01) config font & icons

--- desafio-01-music-player/tailwind.config.js
+++ desafio-01-music-player/tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Roboto', 'sans-serif'],
+      },
+      colors: {
+        violet: {
+          950: '#2A2141',
+        },
+      },
+    },
   },
   plugins: [],
 }
