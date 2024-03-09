9137cb5a83a6b3832435e8beb493c6ad8b3da9d4
Pablo Alan <pabloxt14@gmail.com>
2024-03-09 15:00:00 -0300

    feat: (challenge 01) create base longmusiccard style

--- desafio-01-music-player/src/app.tsx
+++ desafio-01-music-player/src/app.tsx
@@ -1,8 +1,10 @@
+import { LongMusicCard } from './components/long-music-card'
+
 export function App() {
   return (
     <div className="flex h-full min-h-screen w-full items-center justify-center">
-      <div className="grid w-full gap-8 px-4 align-middle md:w-fit md:grid-cols-[266px_357px] md:grid-rows-2">
-        <div className="bg-red-500 md:row-span-2 md:h-[498px]">player 1</div>
+      <div className="grid w-full gap-8 px-4 py-8 align-middle md:w-fit md:grid-cols-[266px_357px] md:grid-rows-2">
+        <LongMusicCard className="md:row-span-2 md:h-[498px]" />
         <div className="bg-green-500">player 2</div>
         <div className="bg-blue-500">player 3</div>
       </div>
---
+++ desafio-01-music-player/src/components/long-music-card/index.tsx
@@ -0,0 +1,61 @@
+import { twMerge } from 'tailwind-merge'
+import bgMusic from '../../assets/images/bg-music.png'
+import { IoPlay, IoPlayBack, IoPlayForward } from 'react-icons/io5'
+
+type LongMusicCardProps = {
+  className?: string
+}
+
+export const LongMusicCard = ({ className }: LongMusicCardProps) => {
+  return (
+    <div
+      className={twMerge(
+        'flex w-full flex-col gap-7 overflow-hidden rounded-lg bg-violet-950 px-9 py-12 md:h-full',
+        className,
+      )}
+    >
+      {/* Card Image */}
+      <div className="h-[190px] w-full overflow-hidden rounded-md">
+        <img src={bgMusic} alt="" className="h-full w-full object-cover" />
+      </div>
+
+      {/* Card Info */}
+      <div>
+        <h2 className="text-2xl font-bold text-zinc-200">Acorda Devinho</h2>
+        <span className="text-xl font-normal text-zinc-200/65">
+          Banda Rocketseat
+        </span>
+      </div>
+
+      {/* Card Actions */}
+      <div className="mx-auto flex items-center gap-5 sm:gap-12 md:mx-0 md:justify-between">
+        <button className="text-zinc-200">
+          <IoPlayBack size={28} />
+        </button>
+
+        <button className="text-zinc-200">
+          <IoPlay size={28} />
+        </button>
+
+        <button className="text-zinc-200">
+          <IoPlayForward size={28} />
+        </button>
+      </div>
+
+      {/* Card Timeline */}
+      <div className="flex flex-col gap-2.5">
+        <progress
+          id="file"
+          value="90"
+          max="100"
+          className="h-1.5 w-full overflow-hidden rounded-full [&::-webkit-progress-bar]:bg-zinc-300/30 [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-zinc-300"
+        />
+
+        <div className="flex items-center justify-between">
+          <span className="text-sm font-normal text-zinc-400">03:20</span>
+          <span className="text-sm font-normal text-zinc-400">00:12</span>
+        </div>
+      </div>
+    </div>
+  )
+}
