552450c8b3965e7d47488c8da535fdc71baba915
Pablo Alan <pabloxt14@gmail.com>
2024-03-13 15:00:00 -0300

    feat[challenge-01]: made music card functional

--- desafio-01-music-player/src/app.tsx
+++ desafio-01-music-player/src/app.tsx
@@ -1,12 +1,21 @@
 import { LongMusicCard } from './components/long-music-card'
 import { MiddleMusicCard } from './components/middle-music-card'
 import { ShortMusicCard } from './components/short-music-card'
+// import music2 from './assets/musics/Never Stop Learning - Do While 2020 Theme.mp3'
+import musicUrl01 from './assets/musics/Build The Future - DoWhile 2021 Theme.mp3'
+import { Music } from './components/@types/music'
 
 export function App() {
+  const music1: Music = {
+    url: musicUrl01,
+    name: 'Never Stop Learning',
+    artist: 'Banda Rocketseat',
+  }
+
   return (
     <div className="flex h-full min-h-screen w-full items-center justify-center">
       <div className="grid w-full gap-8 px-4 py-8 align-middle md:w-fit md:grid-cols-[266px_357px] md:grid-rows-[auto_1fr]">
-        <LongMusicCard className="md:row-span-2 md:h-[498px]" />
+        <LongMusicCard className="md:row-span-2 md:h-[498px]" music={music1} />
         <MiddleMusicCard className="h-fit" />
         <ShortMusicCard className="" />
       </div>
--- desafio-01-music-player/src/components/long-music-card/index.tsx
+++ desafio-01-music-player/src/components/long-music-card/index.tsx
@@ -1,12 +1,71 @@
 import { twMerge } from 'tailwind-merge'
 import bgMusic from '../../assets/images/bg-music.png'
-import { IoPlay, IoPlayBack, IoPlayForward } from 'react-icons/io5'
+import {
+  IoPlay,
+  IoPlayBack,
+  IoPlayForward,
+  IoPause,
+  IoVolumeMedium,
+  IoVolumeMute,
+} from 'react-icons/io5'
+import { ChangeEvent, useEffect, useRef, useState } from 'react'
+import { formatTime } from '../../utils/formate-time'
+import { Music } from '../@types/music'
 
 type LongMusicCardProps = {
   className?: string
+  music: Music
 }
 
-export const LongMusicCard = ({ className }: LongMusicCardProps) => {
+export const LongMusicCard = ({ className, music }: LongMusicCardProps) => {
+  const audioRef = useRef<HTMLAudioElement | null>(null)
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [currentTime, setCurrentTime] = useState(0)
+  const [duration, setDuration] = useState(0)
+  const [volume, setVolume] = useState(0.4)
+
+  const togglePlayPause = () => {
+    if (isPlaying) {
+      audioRef?.current?.pause()
+    } else {
+      audioRef?.current?.play()
+    }
+
+    setIsPlaying((prevState) => !prevState)
+  }
+
+  const handleTimeUpdate = () => {
+    setCurrentTime(audioRef?.current?.currentTime || 0)
+  }
+
+  const handleLoadedData = () => {
+    setDuration(audioRef?.current?.duration || 0)
+  }
+
+  const handleSeek = (event: ChangeEvent<HTMLInputElement>) => {
+    if (!audioRef?.current) return
+
+    const seekTime = Number(event.target.value)
+    audioRef.current.currentTime = seekTime ?? 0
+    setCurrentTime(seekTime)
+  }
+
+  // const handleVolumeChange = (event: ChangeEvent<HTMLInputElement>) => {
+  //   const newVolume = parseFloat(event.target.value)
+
+  //   setVolume(newVolume)
+  // }
+
+  if (audioRef.current) {
+    audioRef.current.volume = volume
+  }
+
+  useEffect(() => {
+    if (currentTime === duration) {
+      setIsPlaying(false)
+    }
+  }, [currentTime, duration])
+
   return (
     <div
       className={twMerge(
@@ -21,9 +80,9 @@ export const LongMusicCard = ({ className }: LongMusicCardProps) => {
 
       {/* Card Info */}
       <div>
-        <h2 className="text-2xl font-bold text-zinc-200">Acorda Devinho</h2>
+        <h2 className="text-2xl font-bold text-zinc-200">{music.name}</h2>
         <span className="text-xl font-normal text-zinc-200/65">
-          Banda Rocketseat
+          {music.artist}
         </span>
       </div>
 
@@ -33,8 +92,8 @@ export const LongMusicCard = ({ className }: LongMusicCardProps) => {
           <IoPlayBack size={28} />
         </button>
 
-        <button className="text-zinc-200">
-          <IoPlay size={28} />
+        <button className="text-zinc-200" onClick={togglePlayPause}>
+          {isPlaying ? <IoPause size={28} /> : <IoPlay size={28} />}
         </button>
 
         <button className="text-zinc-200">
@@ -42,18 +101,48 @@ export const LongMusicCard = ({ className }: LongMusicCardProps) => {
         </button>
       </div>
 
+      {/* Card Audio */}
+      <audio
+        ref={audioRef}
+        src={music.url}
+        onTimeUpdate={handleTimeUpdate}
+        onLoadedData={handleLoadedData}
+      />
+
       {/* Card Timeline */}
       <div className="flex flex-col gap-2.5">
-        <progress
-          id="file"
-          value="90"
-          max="100"
-          className="h-1.5 w-full overflow-hidden rounded-full [&::-webkit-progress-bar]:bg-zinc-300/30 [&::-webkit-progress-value]:rounded-full [&::-webkit-progress-value]:bg-zinc-300"
-        />
+        {/* Card Volume (future) */}
+        {/* <div className="group flex cursor-pointer items-center gap-2">
+          {volume > 0 ? (
+            <IoVolumeMedium size={18} className="text-zinc-200" />
+          ) : (
+            <IoVolumeMute size={18} className="text-zinc-200" />
+          )}
+          <input
+            type="range"
+            min={0}
+            max={1}
+            step={0.01}
+            value={volume}
+            onChange={handleVolumeChange}
+            className="hidden h-1.5 w-full cursor-pointer appearance-none overflow-hidden rounded-full border-none bg-zinc-300/30 outline-none transition-all group-hover:inline-block [&::-webkit-slider-thumb]:h-1.5 [&::-webkit-slider-thumb]:w-1.5 [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-zinc-300 [&::-webkit-slider-thumb]:shadow-[-403px_0_0_400px] [&::-webkit-slider-thumb]:shadow-zinc-300"
+          />
+        </div> */}
 
+        <input
+          type="range"
+          value={currentTime}
+          max={duration}
+          className="h-1.5 w-full cursor-pointer appearance-none overflow-hidden rounded-full border-none bg-zinc-300/30 outline-none [&::-webkit-slider-thumb]:h-1.5 [&::-webkit-slider-thumb]:w-1.5 [&::-webkit-slider-thumb]:cursor-pointer [&::-webkit-slider-thumb]:appearance-none [&::-webkit-slider-thumb]:rounded-full [&::-webkit-slider-thumb]:bg-zinc-300 [&::-webkit-slider-thumb]:shadow-[-403px_0_0_400px] [&::-webkit-slider-thumb]:shadow-zinc-300"
+          onChange={handleSeek}
+        />
         <div className="flex items-center justify-between">
-          <span className="text-sm font-normal text-zinc-400">03:20</span>
-          <span className="text-sm font-normal text-zinc-400">00:12</span>
+          <span className="text-sm font-normal text-zinc-400">
+            {formatTime(currentTime)}
+          </span>
+          <span className="text-sm font-normal text-zinc-400">
+            {formatTime(duration)}
+          </span>
         </div>
       </div>
     </div>
