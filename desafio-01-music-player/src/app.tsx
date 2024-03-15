389c6668c2ad47ad83218c63ad8b722ab858746f
Pablo Alan <pabloxt14@gmail.com>
2024-03-14 23:53:49 -0300

    feat[challenge-01]: add use-music on middle-music-card

--- desafio-01-music-player/src/app.tsx
+++ desafio-01-music-player/src/app.tsx
@@ -1,8 +1,8 @@
 import { LongMusicCard } from './components/long-music-card'
 import { MiddleMusicCard } from './components/middle-music-card'
 import { ShortMusicCard } from './components/short-music-card'
-// import music2 from './assets/musics/Never Stop Learning - Do While 2020 Theme.mp3'
 import musicUrl01 from './assets/musics/Build The Future - DoWhile 2021 Theme.mp3'
+import musicUrl02 from './assets/musics/Never Stop Learning - Do While 2020 Theme.mp3'
 import { Music } from './components/@types/music'
 
 export function App() {
@@ -12,11 +12,17 @@ export function App() {
     artist: 'Banda Rocketseat',
   }
 
+  const music2: Music = {
+    url: musicUrl02,
+    name: 'Build The Future',
+    artist: 'Banda Rocketseat',
+  }
+
   return (
     <div className="flex h-full min-h-screen w-full items-center justify-center">
       <div className="grid w-full gap-8 px-4 py-8 align-middle md:w-fit md:grid-cols-[266px_357px] md:grid-rows-[auto_1fr]">
         <LongMusicCard className="md:row-span-2 md:h-[498px]" music={music1} />
-        <MiddleMusicCard className="h-fit" />
+        <MiddleMusicCard className="h-fit" music={music2} />
         <ShortMusicCard className="" />
       </div>
     </div>
